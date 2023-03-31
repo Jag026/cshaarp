9d42ba15bfad97f381e53875558e076804602788
Drew Griffin <drew.griffin.dev@gmail.com>
2023-03-30 23:44:57 -0500

    new proj

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int players = 12;
+
+string penalty = players > 11 ? "Is Penalty" : "Not a penalty";
+
+Console.WriteLine($"{penalty}");
