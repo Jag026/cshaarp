8ac99ed9692e1bb4c8b0e638254f1e51fdcccad0
Drew Griffin <drew.griffin.dev@gmail.com>
2023-03-31 21:37:29 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿int firstValue = 500;
 int secondValue = 600;
 int largerValue = Math.Max(firstValue, secondValue);
+bool statement = firstValue % 2 == 0 ? true : false;
 
-Console.WriteLine(largerValue);
+Console.WriteLine(statement);
