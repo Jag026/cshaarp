cbd05441d596afe906b5005d0a9a55d834c9f489
Drew Griffin <drew.griffin.dev@gmail.com>
2023-04-04 22:15:43 -0500

    Reandom

--- Program.cs
+++ Program.cs
@@ -1,18 +1,7 @@
-﻿int employeeLevel = 300;
-string employeeName = "Bilbo";
+﻿Random random = new Random();
+int current = 0;
 
-string title = "";
-
-switch (employeeLevel) {
-    case 100:
-        title = "mail room";
-        break;
-    case 200:
-        title = "manager";
-        break;
-    case 300:
-        title = "CEO";
-        break;
-}
-
-Console.WriteLine($"{employeeName}, {title}");
+do {
+    current = random.Next(1, 100);
+    Console.WriteLine(current);
+} while (current != 7);
