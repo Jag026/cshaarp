69d68674cb4eefe3071bdc11af4775b1f5d00f8b
Drew Griffin <drew.griffin.dev@gmail.com>
2023-04-04 22:20:52 -0500

    Reandom

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿Random random = new Random();
+﻿Random dice = new Random();
 int current = 0;
-
+int rollCount = 0;
+Console.WriteLine("RN || Roll");
 do {
-    current = random.Next(1, 100);
-    Console.WriteLine(current);
+    current = dice.Next(2, 12);
+    rollCount++;
+    Console.WriteLine($"{rollCount}       || ${current}");
 } while (current != 7);
+
+Console.WriteLine("Total Rolls:");
+Console.WriteLine(rollCount);
