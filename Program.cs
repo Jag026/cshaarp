155a06746e297a2cddce594704766ad2b549f6ec
Drew Griffin <drew.griffin.dev@gmail.com>
2023-04-06 23:21:13 -0500

    adfsag

--- Program.cs
+++ Program.cs
@@ -1,15 +1,2 @@
-﻿Random dice = new Random();
-int current = 0;
-int rollCount = 0;
-Console.WriteLine("RN || Roll");
-do {
-    current = dice.Next(2, 12);
-    rollCount++;
-    Console.WriteLine($"{rollCount}       || ${current}");
-} while (current != 7);
-
-Console.WriteLine("Total Rolls:");
-Console.WriteLine(rollCount);
-
-
-Console.WriteLine(rollCount * 100)
+﻿Console.WriteLine(Math.Pow(9, -3));
+Console.WriteLine(9 / -3);
