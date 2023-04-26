1aef41269215143c362ba2ab506d0e9c678c3fe4
Drew Griffin <drew.griffin.dev@gmail.com>
2023-04-26 16:34:53 -0500

    new

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿int volume = 9;
-int side = 5;
-bool sideWideth = (volume == 3 * side) ? true : false;
-Console.WriteLine(sideWideth);
+﻿using System;
+
+public class Program
+{
+    public static Tuple<int, int> FindMinMax(int[] arr)
+    {
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        return Tuple.Create(min, max);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 5, 3, 8, 2 };
+        Tuple<int, int> minMax = FindMinMax(arr);
+        Console.WriteLine("Min: " + minMax.Item1);
+        Console.WriteLine("Max: " + minMax.Item2);
+    }
+}
