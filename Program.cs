49aab02d48af563c5e7899c8be2d40d4eefd5c65
Drew Griffin <drew.griffin.dev@gmail.com>
2023-04-27 10:55:09 -0500

    new

--- Program.cs
+++ Program.cs
@@ -1,23 +1,10 @@
 ﻿using System;
 
-public class Program
-{
-    public static Tuple<int, int> FindMinMax(int[] arr)
-    {
-        int max = arr[0], min = arr[0];
-        for (int i = 1; i < arr.Length; i++)
-        {
-            if (arr[i] > max) max = arr[i];
-            if (arr[i] < min) min = arr[i];
+class Program {
+    static void Main(string[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        foreach(int num in arr) {
+            Console.WriteLine(num);
         }
-        return Tuple.Create(min, max);
-    }
-
-    static void Main(string[] args)
-    {
-        int[] arr = { 1, 5, 3, 8, 2 };
-        Tuple<int, int> minMax = FindMinMax(arr);
-        Console.WriteLine("Min: " + minMax.Item1);
-        Console.WriteLine("Max: " + minMax.Item2);
     }
 }
