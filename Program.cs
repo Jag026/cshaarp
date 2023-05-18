f0eba1622e7a91267895d3828c2406007cd77748
Drew Griffin <drew.griffin.dev@gmail.com>
2023-05-18 14:22:27 -0500

    Update command line game

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿for (int i = 0; i < 5; i += 1) {
-    Console.WriteLine(i);
+﻿Console.Write("How many times have you watched Space Balls?");
+
+string viewings = Console.ReadLine();
+
+try 
+{
+    int views = int.Parse(viewings);
+    Console.WriteLine(views);
 }
+catch (FormatException)
+{
+    Console.WriteLine("You didn't provide a number");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Too many viewings");
+}
+
